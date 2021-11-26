b085f05a8ac3a7e9f54e17434d433e668a611391
Turismo99 <theresaclarke99@gmail.com>
2021-11-26 14:20:03 -0400

    Made menu & How to play

---
+++ Gravitron/Assets/Scripts/MenuButtonScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Start the game
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 1", LoadSceneMode.Single); // THIS WILL PROBABLY NEED TO BE CHANGED TO PLAY THE INTRO CUTSCENE INSTEAD
+    }
+
+    // Open the How to Play menu
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("How to Play", LoadSceneMode.Single);
+    }
+
+    // Go back to the main menu
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
