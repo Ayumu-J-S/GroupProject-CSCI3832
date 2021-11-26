03e4a61a8552c6409f90e91b58ba0985720955ab
Ayumu-J-S <umoo00015@gmail.com>
2021-11-26 17:02:14 -0400

    Change the program so that when the player is killed, reload the scene
    This way player has to press the button and go to the door without dying. Old setting was that player could press the button and be dead but the door would still be open.

--- Gravitron/Assets/Scripts/MoveSpikesWithButton.cs
+++ Gravitron/Assets/Scripts/MoveSpikesWithButton.cs
@@ -6,17 +6,23 @@ public class MoveSpikesWithButton : MonoBehaviour
 {
     public GameObject SpikesUp;
     public GameObject SpikesDown;
+    private Animator spikesUpAnim;
+    private Animator spikesDownAnim;
 
     // Start is called before the first frame update
     void Start()
     {
-        Animator spikesUpAnim = SpikesUp.GetComponent<Animator>();
-        Animator spikesDownAnim = SpikesUp.GetComponent<Animator>();
+        spikesUpAnim = SpikesUp.GetComponent<Animator>();
+        spikesDownAnim = SpikesDown.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (ButtonScript.buttonPressed)
+        {
+            spikesUpAnim.Play("MoveSpikesUp");
+            spikesDownAnim.Play("MoveSpikesDown");
+        }
     }
 }
