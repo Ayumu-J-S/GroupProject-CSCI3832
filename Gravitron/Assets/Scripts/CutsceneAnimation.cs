1d3646fbadbe8117503d649c6ddb47a08bf72702
Turismo99 <theresaclarke99@gmail.com>
2021-11-28 20:43:00 -0400

    Added script to advance cutscenes and made a new scene for the intro

--- Gravitron/Assets/Scripts/CutsceneAnimation.cs
+++ Gravitron/Assets/Scripts/CutsceneAnimation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CutsceneAnimation : MonoBehaviour
 {
@@ -10,10 +11,20 @@ public class CutsceneAnimation : MonoBehaviour
     public bool isStanding = true;
     public bool inTransition = false;
 
+    // Variables for speech box (Note: This requires that a TextMeshPro called "Speech Text" is in the current scene)
+    GameObject speechTextObject;
+    TextMeshProUGUI speechText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get the text mesh
+        speechTextObject = GameObject.Find("Speech Text");
+        speechText = speechTextObject.GetComponent<TextMeshProUGUI>();
+
+        // Start the cutscene
+        StartCoroutine("Cutscene");
     }
 
     void FixedUpdate()
@@ -62,6 +73,32 @@ public class CutsceneAnimation : MonoBehaviour
 
         inTransition = false;
         animator.SetBool("InTransition", inTransition);
+    }
+
+    // The cutscene can be controlled from here
+    IEnumerator Cutscene()
+    {
+        // Set speech text, check to see if space has been hit, and then wait for a second (to avoid zooming through)
+        speechText.text = "Hit space!";
+        yield return hitSpace();
+        speechText.text = "Thanks! Hit space again!!";
+        yield return hitSpace();
+        speechText.text = "You follow rules really well.";
+        yield return hitSpace();
+        speechText.text = "Well that's it for this placeholder text!";
+
+    }
+
+    // Waits until the space key has been hit
+    private IEnumerator hitSpace()
+    {
+        while (!Input.GetKeyDown(KeyCode.Space))
+        {
+            yield return null;
+        }
+
+        // Wait to prevent one key press spanning multiple frames as counting for multiple key presses
+        yield return new WaitForSeconds(0.2f);
     }
 
 }
